49e1e22530a817b4dd7eca8477a680a4249efdde
Jonathan <void.knight09@gmail.com>
2020-12-10 21:51:29 +1300

    Update

--- example/src/shared/module.ts
+++ example/src/shared/module.ts
@@ -1,11 +1,11 @@
-import { env } from "../../..";
-declare function env<T extends string = string>(name: string): T | undefined;
-declare function env<T extends string = string>(name: string, defaultValue: T): T;
-declare function env<T extends number>(name: string): T | undefined;
-declare function env<T extends number>(name: string, defaultValue: T): T;
-declare function env<T extends boolean>(name: string): T | undefined;
-declare function env<T extends boolean>(name: string, defaultValue: T): T;
-declare function ifEnv(name: string, matches: string, fn: () => void): void;
+import { env, ifEnv } from "../../..";
+// declare function env<T extends string = string>(name: string): T | undefined;
+// declare function env<T extends string = string>(name: string, defaultValue: T): T;
+// declare function env<T extends number>(name: string): T | undefined;
+// declare function env<T extends number>(name: string, defaultValue: T): T;
+// declare function env<T extends boolean>(name: string): T | undefined;
+// declare function env<T extends boolean>(name: string, defaultValue: T): T;
+// declare function ifEnv(name: string, matches: string, fn: () => void): void;
 
 
 export function makeHello(name: string) {
